80747f2fb7ca1417ff7f8d14c5e0727658b4d838
Anirudh rai <developer18@flickios.com>
2026-04-10 07:02:22 +0530

    Remove unused useCallback import

--- convoia_frontend/src/components/VoiceConversationMode.tsx
+++ convoia_frontend/src/components/VoiceConversationMode.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { X, Mic } from 'lucide-react'
 import { useVoiceConversation } from '../hooks/useVoiceConversation'
 
